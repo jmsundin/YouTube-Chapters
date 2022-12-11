d06ddbd368eca0a3615d2680823b076756f5dfd9
Jon Sundin <sundinjon@gmail.com>
2022-12-10 19:52:12 -0800

    base dev env running

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import "./App.css";
+
+const App = () => {
+    
+    return (
+        <div>
+            <h1>YouTube Chapters</h1>
+            <p>
+                This is a simple React app that allows you to create chapters for your YouTube videos.
+            </p>
+        </div>
+    )
+};
+
+export default App;
